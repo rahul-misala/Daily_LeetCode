55815fac8ebf1b3f01e8f96a20e4cfef0401e129
Rahul_Misala <rahulmeesala416@gmail.com>
2025-02-19 00:26:50 +0530

    Create Construct_Smallest_Number_From_DI_String.cpp

---
+++ LC-2375-STR-M-18-FEB-25/Construct_Smallest_Number_From_DI_String.cpp
@@ -0,0 +1,30 @@
+//leetcode link : https://leetcode.com/problems/construct-smallest-number-from-di-string/description/
+
+
+class Solution {
+    public:
+        string smallestNumber(string pattern) {
+            int n = pattern.size();
+            stack<int> st;
+            string res;
+            int count = 1;
+            for(int i=0;i<n;i++){
+                st.push(count);
+                if(pattern[i]=='I'){
+                    while(!st.empty()){
+                        char num = st.top();
+                        res.push_back(char(num+48));
+                        st.pop();
+                    }
+                }
+                count++;
+            }
+            st.push(count);
+            while(!st.empty()){
+                char num = st.top();
+                res.push_back(char(num+48));
+                st.pop();
+            }
+            return res;
+        }
+    };
